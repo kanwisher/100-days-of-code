0458cb50ccada565ce0099ff53a54a06e03eef8c
David Kanwisher <kanwisher@gmail.com>
2017-01-23 00:15:52 -0500

    Update List program vanilla JS

--- start/app.js
+++ start/app.js
@@ -6,6 +6,7 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 
+
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
@@ -18,16 +19,17 @@ toggleList.addEventListener('click', () => {
 
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = "";
 });
 
 addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = "";
 });
-  
-  
-  
-  
+
   
   
   
